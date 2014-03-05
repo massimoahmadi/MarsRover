4c3cbfc93582060874da4d281358022df78595c6
Massimo Hamilton <massimo.hamilton@outlook.com>
2014-03-05 21:33:56 +0000

    Add failing Unit Tests to highlight that the MoveBackward command is only moving South, regardless of the current direction

--- MarsRover/UnitTests.cs
+++ MarsRover/UnitTests.cs
@@ -136,5 +136,53 @@ namespace MarsRover
 			rover.Move("F");
 			Assert.AreEqual("-1, 0, W", rover.CurrentPosition);
 		}
+
+		[TestMethod]
+		public void When_A_Rover_Is_Facing_North_And_Given_The_Command_B_The_Position_Should_Be_0_Minus1_N()
+		{
+			var rover = new MarsRover();
+
+			Assert.AreEqual("0, 0, N", rover.CurrentPosition);
+
+			rover.Move("B");
+			Assert.AreEqual("0, -1, N", rover.CurrentPosition);
+		}
+
+		[TestMethod]
+		public void When_A_Rover_Is_Facing_East_And_Given_The_Command_B_The_Position_Should_Be_Minus1_0_E()
+		{
+			var rover = new MarsRover();
+
+			rover.Move("R");
+			Assert.AreEqual("0, 0, E", rover.CurrentPosition);
+
+			rover.Move("B");
+			Assert.AreEqual("-1, 0, E", rover.CurrentPosition);
+		}
+
+		[TestMethod]
+		public void When_A_Rover_Is_Facing_South_And_Given_The_Command_B_The_Position_Should_Be_0_1_S()
+		{
+			var rover = new MarsRover();
+
+			rover.Move("R");
+			rover.Move("R");
+			Assert.AreEqual("0, 0, S", rover.CurrentPosition);
+
+			rover.Move("B");
+			Assert.AreEqual("0, 1, S", rover.CurrentPosition);
+		}
+
+		[TestMethod]
+		public void When_A_Rover_Is_Facing_West_And_Given_The_Command_B_The_Position_Should_Be_1_0_E()
+		{
+			var rover = new MarsRover();
+
+			rover.Move("L");
+			Assert.AreEqual("0, 0, W", rover.CurrentPosition);
+
+			rover.Move("B");
+			Assert.AreEqual("1, 0, W", rover.CurrentPosition);
+		}
 	}
 }

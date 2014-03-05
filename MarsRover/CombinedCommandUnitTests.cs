ed6d484a09f0b19a3b20062f7e7aeb36bcfa6ab7
Massimo Hamilton <massimo.hamilton@outlook.com>
2014-03-05 21:39:00 +0000

    Add CombinedCommandUnitTests with a failing unit test for testing strings of commands

---
+++ MarsRover/CombinedCommandUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsRover
+{
+	[TestClass]
+	public class CombinedCommandUnitTests
+	{
+		[TestMethod]
+		public void When_A_New_Rover_Is_Given_The_Command_FF_The_Position_Should_Be_0_2_N()
+		{
+			var rover = new MarsRover();
+
+			rover.Move("FF");
+
+			Assert.AreEqual("0, 2, N", rover.CurrentPosition);
+		}
+	}
+}

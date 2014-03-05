41d4297e02686220584d5b229c6ce219d7679e29
Massimo Hamilton <massimo.hamilton@outlook.com>
2014-03-05 11:02:42 -0800

    Initial commit

---
+++ MarsRover/MarsRover.cs
@@ -0,0 +1,24 @@
+﻿namespace MarsRover
+{
+	public class MarsRover
+	{
+		// Could use properties if public access required.
+		private int _x;
+		private int _y;
+		private Direction _direction;
+
+		public void Move(string command)
+		{
+		}
+
+		public string CurrentPosition
+		{
+			get
+			{
+				{
+					return "";
+				}
+			}
+		}
+	}
+}
---
+++ MarsRover/UnitTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsRover
+{
+	[TestClass]
+	public class UnitTests
+	{
+		[TestMethod]
+		public void When_A_New_Rover_Is_Created_The_Position_Should_Be_0_0_N()
+		{
+			var rover = new MarsRover();
+
+			Assert.AreEqual("0, 0, N", rover.CurrentPosition);
+		}
+	}
+}
